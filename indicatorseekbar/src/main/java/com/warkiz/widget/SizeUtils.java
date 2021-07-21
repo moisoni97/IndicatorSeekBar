e2b32d16f07378ca3aa98ecf45a757c4d982cbfc
Moisoni Ioan <moisoniioan@gmail.com>
2021-07-21 15:14:38 +0300

    Code cleanup

--- indicatorseekbar/src/main/java/com/warkiz/widget/SizeUtils.java
+++ indicatorseekbar/src/main/java/com/warkiz/widget/SizeUtils.java
@@ -3,11 +3,8 @@ package com.warkiz.widget;
 import android.content.Context;
 import android.util.TypedValue;
 
-/**
- * created by zhuangguangquan on  2017/9/9
- */
-
 public class SizeUtils {
+
     public static int dp2px(Context context, float dpValue) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, context.getResources().getDisplayMetrics());
     }
@@ -19,5 +16,4 @@ public class SizeUtils {
     public static int px2sp(Context context, float pxValue) {
         return (int) (pxValue / context.getResources().getDisplayMetrics().scaledDensity + 0.5f);
     }
-
 }
